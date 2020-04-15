dbbe78af18d53d682dbebe0d325ddbc86c31deda
alemanjosh7 <40409070+alemanjosh7@users.noreply.github.com>
2020-04-15 17:42:23 -0600

    Initializing the project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+// Settings
+
+
+
+// Middlewares
+
+
+
+// Static Files
+
+
+
+// Starting the server
+
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+})
