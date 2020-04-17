1de1bd6f8378e34a62e8a56c1733841f633d058d
alemanjosh7 <40409070+alemanjosh7@users.noreply.github.com>
2020-04-16 18:10:37 -0600

    Creating components in React

---
+++ frontend/src/components/CreateBook.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateBook extends Component {
+    render() {
+        return (
+            <div>
+                Create Book
+            </div>
+        )
+    }
+}
