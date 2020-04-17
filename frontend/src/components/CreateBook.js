742cfb876eb6ba80759a0d628e74a84981f34384
alemanjosh7 <40409070+alemanjosh7@users.noreply.github.com>
2020-04-16 20:06:25 -0600

    Requesting with Axios

--- frontend/src/components/CreateBook.js
+++ frontend/src/components/CreateBook.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class CreateBook extends Component {
+
+    async componentDidMount(){
+        const res = await axios.get('https://mern-stack-bookshelf.herokuapp.com/api/books');
+        console.log(res);
+    }
+
     render() {
         return (
             <div>
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,36 +1,39 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default class Navigation extends Component {
   render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown link
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <i className="material-icons"></i> Bookshelf App
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">
+                  Books
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">
+                  Upload a Book
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
